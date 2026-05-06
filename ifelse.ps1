0497fd0308f1cc3473a12feffb77108a23798b94
Gleipneer <emilssonjoakim@gmail.com>
2026-05-06 10:23:24 +0200

    variabler

---
+++ ifelse.ps1
@@ -0,0 +1,8 @@
+# kolla att talet är är större än än 5
+
+$tal = 4
+if ($tal -gt 5) {
+    write-host "Talet är större än 5"# kod som körs om condition är sant
+} else { 
+    write-host "Talet är inte större än 5"# kod som körs om condition är falskt
+}
