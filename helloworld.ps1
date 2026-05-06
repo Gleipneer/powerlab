ddd2371b8ad3ba4201f44bbffda836a10cf97a98
Gleipneer <emilssonjoakim@gmail.com>
2026-05-06 09:49:01 +0200

    variabler

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,1 +1,1 @@
-Write-Output "Hello World from PowerShell"
+Write-Host "Hello World from PowerShell"
---
+++ var.ps1
@@ -0,0 +1,6 @@
+#två versaler, namn och ålder, skriv ut i en mening
+
+$name = "Anna"
+$alder = "25"
+
+Write-Host "Hej $name, du är $alder år gammal!"
